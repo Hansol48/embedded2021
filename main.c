45a0927e1fc0c14e6cd6f414a69338afcaacbce4
leehansol <elpatron0408@gmail.com>
2021-11-14 00:02:25 +0900

    plz
    되라좀 ㅠ

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "myProject.h"
+
+int main(void)
+{
+	printf("Chief name is [Seo Byungjun]\n");
+	printMyname1();
+	printMyname2();
+}
---
+++ myProject.h
@@ -0,0 +1,10 @@
+#ifndef _MY_PROJECT_H_
+#define _MY_PROJECT_H_
+
+int printMyname1(void);
+int printMyname2(void);
+
+void lhs(void);
+void sbj(void);
+
+#endif
