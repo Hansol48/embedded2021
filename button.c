5b85f797dfc1927a1e2bbf803f628a5cc41ba8e4
Hansol48 <elpatron0408@gmail.com>
2021-12-05 20:42:40 +0900

    12-2
    12-2

--- button.c
+++ button.c
@@ -9,52 +9,11 @@
 #include <pthread.h>
 #include "button.h"
 
-#define INPUT_DEVICE_LIST "/dev/input/event"
+#define INPUT_DEVICE_LIST "/dev/input/event5"
 #define PROBE_FILE "/proc/bus/input/devices"
 #define HAVE_TO_FIND_1 "N: Name=\"ecube-button\"\n"
 #define HAVE_TO_FIND_2 "H: handlers=kbd event5"
 
-int main(int argc, char *argc[])
-{
-	int fp;
-	int readSize, inputIndex;
-	struct input_event stEvent;
-	char inputDevPath[200]={0, };
-	if ( probeButtonPath(inputDevPath) == 0)
-	{
-		printf("ERROR! File Not Found!\r\n");
-		printf("Did you insmod?\r\n");
-		return 0;
-	}
-	printf("inputDevPatn : %s\r\n", inputDevPath);
-	fp = open(inputDevPath, O_RDONLY);
-	while(1)
-	{
-		readSize = read(fp, &stEvemt, sizeof(stEvent));
-		if (readSize != sizeof(stEvent))
-		{
-			continue;
-		}
-		if ( stEvemt.type == EV_KEY)
-		{
-			printf("EV_KEY(");
-			switch(stEvent.code)
-			{
-				case KEY_VOLUMEUP: printf("Volume up key):"); break;
-				case KEY_HOME: printf("Home key):)"); break;
-				case KEY_SEARCH: printf("Search key):"); break;
-				case KEY_BACK: prinf("Back key):"); break;
-			     	case KEY_MENU: prinf("Menu key):"); break;
-				case KEY_VOLUMEDOWN: printf("Volume down key):"); break;
-			}	
-			if ( stEvent.calue ) printf("pressed\n");
-			else printf("released\n");
-		}
-		else
-		;
-	}
-	close(fp);
-}
      
 int probeButtonPath(char *newPath)
 {
